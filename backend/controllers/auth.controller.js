af2f88b44873c398860eb4f30a95b2e82475a210
LinearDash <asandesh495@gmail.com>
2024-12-12 23:01:58 +0545

    MongoDB setup with env Variable

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+export const signup = async (req,res)=>
+  {
+  res.json({
+   data:"Signup Endpoint hit"
+  });
+}
+
+export const login = async (req,res)=>
+  {
+  res.json({
+   data:"login Endpoint hit"
+  });
+}
+
+export const logout = async (req,res)=>
+  {
+  res.json({
+   data:"logout Endpoint hit"
+  });
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("server is running");
+
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js"
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+  //Running at port 5000
+  console.log(`server is running on port : ${PORT}`);
+  connectMongoDB();
+  
+})
