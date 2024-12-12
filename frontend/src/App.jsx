310d21dbd8445d5daf44f31d7a0b6642ae971dc9
LinearDash <asandesh495@gmail.com>
2024-12-12 18:14:04 +0545

    reorder files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-xl text-center text-slate-600">
+        <p>Twitter CLone</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
