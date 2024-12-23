b8e3ee440d893ab747a17888c34ed459d3e94817
LinearDash <asandesh495@gmail.com>
2024-12-23 19:24:28 +0545

    add deletePost controller and route; implement authorization and Cloudinary image deletion

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -33,3 +33,31 @@ export const createPost = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    // Checks if the user is authorized to delete the post
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this post" });
+    }
+
+    // If there is an image in the post, it will be deleted from Cloudinary
+    if (post.img) {
+      // Splitting the Cloudinary link to get only the image ID
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
