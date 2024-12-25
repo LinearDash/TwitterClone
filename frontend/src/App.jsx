411c5f88e753e1cb17c8790207912f0ba345648d
LinearDash <asandesh495@gmail.com>
2024-12-25 21:50:18 +0545

    add loading and skeleton components; implement RightPanel and update HomePage layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,23 @@ import "./App.css";
 import HomePage from "./pages/Home/HomePage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
+import SideBar from "./components/common/SideBar";
+import RightPannel from "./components/common/RightPannel";
+import NotificationPage from "./pages/Notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <SideBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPannel />
     </div>
   );
 }
