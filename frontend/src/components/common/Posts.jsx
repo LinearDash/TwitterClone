f44df95297cedf82ef1f45ef37b52e6cabfa9e87
LinearDash <asandesh495@gmail.com>
2024-12-28 22:03:41 +0545

    for user feed and following feed working

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,25 +1,69 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedType }) => {
+  // const isLoading = false;
 
+  // To see which feed Type user is on and select a endpoint in the basis of that
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "/api/post/all";
+      case "following":
+        return "/api/post/following";
+      default:
+        return "/api/post/all";
+    }
+  };
+
+  const POST_ENDPOINT = getPostEndpoint();
+
+  //
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch]);
   return (
     <>
-      {isLoading && (
+      {/* If the posts are loading or refetching this wil show a skeleton */}
+      {(isLoading || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {/* IF it is not loading the post, not refeching the posts and there doesn't exist any post  */}
+      {!isLoading && !isRefetching && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {/* If its not loading the post, not refeching the posts and there exists a number of post */}
+      {!isLoading && !isRefetching && posts && (
         <div>
-          {POSTS.map((post) => (
+          {posts.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
